6847175a772751e07e271e8d016231454ce5b1dd
Vitor Ramos <vitor.ramos2759@gmail.com>
2022-12-12 11:38:14 -0300

    initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import string
+import random
+
+characters = list(string.ascii_letters + string.digits + "!@#$%^&*()")
+
+
+def generate_password():
+        password_length = int(input("How long is the password? "))
+        random.shuffle(characters)
+        password = []
+
+        for x in range(password_length):
+            password.append(random.choice(characters))
+
+        random.shuffle(password)
+
+        password = "".join(password)
+
+        print(password)
+
+
+option = input("Would you like a random password for your security? (Yes/No): ").capitalize()
+if option == "Yes":
+    generate_password()
+elif option == "No":
+    print("Okay, have a good day :)")
+    quit()
+else:
+    print("Invalid option, please type Y/N")
+    quit()
