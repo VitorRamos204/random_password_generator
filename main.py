9af13e7a6f1b5ef1e9b99cba4c0401d750c162f4
Vitor Ramos <vitor.ramos2759@gmail.com>
2022-12-12 11:42:25 -0300

    refactor and new comments

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import string
 import random
 
+# array of all characters on keyboard
 characters = list(string.ascii_letters + string.digits + "!@#$%^&*()")
 
 
 def generate_password():
-        password_length = int(input("How long is the password? "))
-        random.shuffle(characters)
-        password = []
+    # Generate the password based on user input
+    password_length = int(input("How long is the password? "))
+    random.shuffle(characters)
+    password = []
 
-        for x in range(password_length):
-            password.append(random.choice(characters))
+    for x in range(password_length):
+        password.append(random.choice(characters))
 
         random.shuffle(password)
 
@@ -19,6 +21,7 @@ def generate_password():
         print(password)
 
 
+# Ask the user if he wants a password or not
 option = input("Would you like a random password for your security? (Yes/No): ").capitalize()
 if option == "Yes":
     generate_password()
